8c6890d827f3044fdc7b8cccbb047faa05b25e58
Steve Pieper <pieper@isomics.com>
2021-05-14 13:02:36 -0400

    Adapt for AppEngine
    Don't start serving until token is received so that the first
    requests aren't served until the token is ready.

--- server.js
+++ server.js
@@ -3,18 +3,32 @@
  * https://joshuatz.com/posts/2019/google-cloud-functions-http-proxy-relay-with-http-proxy-middleware/
  */
 const {google} =require('googleapis');
+
 const express = require('express');
 const proxyMiddleware = require('http-proxy-middleware');
 
 const port = process.env.PORT || 8080;
 
+//const dicomURL = process.env.DICOMURL || 'https://healthcare.googleapis.com/v1beta1/projects/project-7519307760985532298/locations/us-central1/datasets/SampleData/dicomStores/dicomstore1/dicomWeb/';
 const dicomURL = process.env.DICOMURL || 'https://healthcare.googleapis.com/v1beta1/projects/idc-external-006/locations/us-central1/datasets/hackathon/dicomStores/2021-ecdp/dicomWeb/';
 
+let app;
 let token = null;
+
+function runApp() {
+  app = express();
+  app.all('*', getProxy());
+
+  app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+  });
+}
 
 function getToken() {
   google.auth.getAccessToken().then(accessToken => {
+    console.log("got token", accessToken);
     token = accessToken;
+    runApp();
     // new token every 30 minutes
     setTimeout(getToken, 1000 * 60 * 30);
   }).catch(error => {
@@ -25,9 +39,6 @@ function getToken() {
   });
 }
 getToken();
-
-const app = express();
-app.all('*', getProxy());
 
 function getProxy() {
   const dicomProxy = proxyMiddleware({
@@ -42,6 +53,3 @@ function getProxy() {
   return dicomProxy;
 }
 
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
-});
